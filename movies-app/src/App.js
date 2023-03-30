328ae1813bf2cfebb94a3116175e20d3609355c0
BitPT <phamtobit123@gmail.com>
2023-03-30 22:56:25 +0700

    update1

---
+++ movies-app/src/App.js
@@ -0,0 +1,43 @@
+import { Fragment } from "react";
+import { NavLink } from "react-router-dom";
+
+function App() {
+  return (
+    <Fragment>
+      <header className="header flex items-center justify-center gap-x-5 text-white py-10">
+        <span className="text-primary">Home</span>
+        <span>Movies</span>
+      </header>
+
+      <section className="banners h-[400px] page-containers">
+        <div className="w-full h-full rounded-lg relative">
+        <div className="overlay absolute inset-0 bg-gradient-to-t from-[rgba(0,0,0,0.5)] to-[rgba(0,0,0,0.5)] rounded-lg"></div>
+          <img
+            src="https://allears.net/wp-content/uploads/2020/10/Avengers-Infinity-War-Poster.jpg"
+            alt=""
+            className="w-full h-full object-cover rounded-lg"
+          />
+          <div className="absolute left-5 bottom-5 w-full text-white">
+            <h2 className="font-bold text-3xl mb-5">Avengers: Endgame.</h2>
+            <div className="flex items-center gap-x-3 mb-8">
+              <span className="py-2 px-4 border border-white rounded-md">
+                Action
+              </span>
+              <span className="py-2 px-4 border border-white rounded-md">
+                Adventure
+              </span>
+              <span className="py-2 px-4 border border-white rounded-md">
+                Drama
+              </span>
+            </div>
+            <button className="btn-watchnow bg-primary">
+              Watch Now
+            </button>
+          </div>
+        </div>
+      </section>
+    </Fragment>
+  );
+}
+
+export default App;
